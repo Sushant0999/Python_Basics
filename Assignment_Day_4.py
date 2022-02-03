18977001f6feceba64b6b08d96becc9ad4bd7afb
Sushant Raj <66193982+Sushant0999@users.noreply.github.com>
2022-02-03 23:16:00 +0530

    Updated Assignment Day 4 Notes Added

--- Assignment_Day_4.py
+++ Assignment_Day_4.py
@@ -1,5 +1,8 @@
-#Assignment Day 4
-
+# Assignment Day 4
+# Edit: Added Assignment Question
+# Day 4 Assignment:
+# Define a function which will take a number as argument and return its factorial.
+# Call the function to print factorial of any number(integer).
 
 #Function
 def fact(num):
@@ -13,3 +16,7 @@ value = int(input("Enter an integer: "))
 
 #Calling Function
 print("Factorial of", value, "is :", fact(value))
+
+
+# Notes 
+# expected 2 blank lines after class or function definition in python
